f904476b3dd876b2b5385cbe6e2cef2e9fffa94d
MaulanaGilang <91357121+MaulanaGilang@users.noreply.github.com>
2023-06-08 12:10:45 +0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -21,10 +21,84 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
-const userRoute =  require("./route/user");
-const databaseRoute =  require("./route/database");
-app.use("/register", userRoute);
-app.use("/data", databaseRoute);
+app.post("/register", async (req, res) => {
+  try {
+    // Extract the required data from the request body
+    console.log(req);
+    const { email, username, password, phoneNumber } = req.body;
+
+    // Check if all required fields are present
+    if (!email) {
+      return res.status(400).json({ message: "Email is required" });
+    }
+
+    if (!username) {
+      return res.status(400).json({ message: "Username is required" });
+    }
+
+    if (!password) {
+      return res.status(400).json({ message: "Password is required" });
+    }
+
+    if (!phoneNumber) {
+      return res.status(400).json({ message: "Phone number is required" });
+    }
+
+    // Validate email format using a regular expression
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    // Validate password length
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password should be at least 6 characters long" });
+    }
+
+    // Generate a unique ID using nanoid
+    const id = nanoid();
+
+    // Create a new user object with the generated ID
+    const newUser = {
+      id,
+      email,
+      username,
+      password,
+      phoneNumber,
+    };
+
+    // Store the user data in Firebase Firestore
+    await usersRef.doc(id).set(newUser);
+
+    // Return a response indicating success
+    res.json({ message: "User registered successfully" });
+  } catch (error) {
+    // Return a response indicating failure
+    res.status(500).json({ message: "Failed to register user" });
+  }
+});
+
+// Endpoint to get data from Firestore
+app.get("/data", (req, res) => {
+  const collectionRef = db.collection("databaseDataRaw");
+
+  collectionRef
+    .get()
+    .then((snapshot) => {
+      const data = [];
+      snapshot.forEach((doc) => {
+        const { target, texts } = doc.data();
+        data.push({ target, texts });
+      });
+      res.json(data);
+    })
+    .catch((error) => {
+      console.error("Error getting data from Firestore:", error);
+      res.status(500).send("Internal Server Error");
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
