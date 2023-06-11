c30b22d87657b763ca19ce0be80ef66614151e4c
Gilang <maulana.gilang305@gmail.com>
2023-06-11 21:44:55 +0700

    ranigay

--- src/controllers/searchedController.ts
+++ src/controllers/searchedController.ts
@@ -1,29 +1,40 @@
-import { db } from "../config/firebase";
 import { Request, Response } from "express";
 import { nanoid } from "nanoid";
+import { db } from "../config/firebase";
+
+interface Place {
+  id: string;
+  place: string;
+}
 
 export const searchedByUser = async (req: Request, res: Response) => {
   try {
     // Extract the required data from the request body
-    const { place } = req.body;
+    const { place, userId } = req.body;
 
-    // Generate a unique ID using nanoid
+    // Generate a unique ID for the subcollection document using nanoid
     const id = nanoid();
 
-    // Create a new user object with the generated ID
-    const newPlace = {
+    // Create a new place object with the generated ID
+    const newPlace: Place = {
       id,
       place,
     };
 
-    // Store the user data in Firebase Firestore
-    await db.collection("databaseSearchedByUser").doc(id).set(newPlace);
+    // Reference the parent collection document
+    const userRef = db.collection("databaseUser").doc(userId);
+
+    // Create the subcollection reference within the parent document
+    const subcollectionRef = userRef.collection("databaseSearchedByUser");
+
+    // Store the place data in the subcollection
+    await subcollectionRef.doc(id).set(newPlace);
 
     // Return a response indicating success
-    res.json({ message: "User registered successfully" });
+    res.json({ message: "Place registered successfully" });
   } catch (error) {
     console.log(error);
     // Return a response indicating failure
-    res.status(500).json({ message: "Failed to register user" });
+    res.status(500).json({ message: "Failed to register place" });
   }
 };
