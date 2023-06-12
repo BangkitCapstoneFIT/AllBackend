f513431cf8a7b170df8d7e2ba199c742d9b16bb1
MaulanaGilang <91357121+MaulanaGilang@users.noreply.github.com>
2023-06-12 21:49:24 +0700

    Update userRoutes.ts

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response } from "express";
 import { registerUser } from "../controllers/registerController";
 import { loginUser } from "../controllers/loginController";
-import { searchedByUser } from "../controllers/searchedController";
+import { searchedByUser } from "../controllers/getallwisataController";
 
 const router = express.Router();
 
+router.post("/search", searchedByUser);
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.post("/searched", searchedByUser);
 
 export default router;
