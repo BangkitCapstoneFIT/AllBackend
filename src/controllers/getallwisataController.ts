ca6dadba4e3810cd2c01b2aeb130ee1669c19221
MaulanaGilang <91357121+MaulanaGilang@users.noreply.github.com>
2023-06-12 21:50:11 +0700

    Update getallwisataController.ts

--- src/controllers/getallwisataController.ts
+++ src/controllers/getallwisataController.ts
@@ -1,5 +1,15 @@
+import { Request, Response } from "express";
+import { nanoid } from "nanoid";
+import { db } from "../config/firebase";
 import axios from 'axios';
-import { Request, Response } from 'express';
+import jwt from "jsonwebtoken";
+
+
+
+interface Place {
+  id: string;
+  place: string;
+}
 
 export const getAllWisata = (req: Request, res: Response) => {
   const apiUrl = 'https://maps.googleapis.com/maps/api/place/textsearch/json';
@@ -7,19 +17,58 @@ export const getAllWisata = (req: Request, res: Response) => {
   const apiKey = process.env.API_KEY; // Replace with your actual API key
 
   // Include query parameters in the API request
-  axios.get(apiUrl, {
-    params: {
-      query: query,
-      key: apiKey
-    }
-  })
-    .then(response => {
+  axios
+    .get(apiUrl, {
+      params: {
+        query: query,
+        key: apiKey,
+      },
+    })
+    .then((response) => {
       // Handle the response data
       res.json(response.data);
     })
-    .catch(error => {
+    .catch((error) => {
       // Handle the error
       console.error(error);
       res.status(500).send('Internal Server Error');
     });
 };
+
+export const searchedByUser = async (req: Request, res: Response) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    // Verify and decode the token to get the user ID
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET) as { userId: string };
+
+    // Extract the required data from the request body
+    const { place } = req.body;
+
+    // Generate a unique ID for the subcollection document using nanoid
+    const id = nanoid();
+
+    // Create a new place object with the generated ID
+    const newPlace: Place = {
+      id,
+      place,
+    };
+
+    // Reference the parent collection document
+    const userRef = db.collection("databaseUser").doc(decodedToken.userId);
+
+    // Create the subcollection reference within the parent document
+    const subcollectionRef = userRef.collection("databaseSearchedByUser");
+
+    // Store the place data in the subcollection
+    await subcollectionRef.doc(id).set(newPlace);
+
+    // Return a response indicating success
+    res.json({ message: "Place registered successfully" });
+  } catch (error) {
+    console.log(error);
+    // Return a response indicating failure
+    res.status(500).json({ message: "Failed to register place" });
+  }
+};
+
